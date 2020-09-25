c5c2652588fef70e46d5d688420085ba62817879
isPlaying <369370@gmail.com>
2020-09-25 20:59:41 +0800

    feat(*): 安装chai,chai-spies编写单元测试

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
+import chai from 'chai';
+import spies from 'chai-spies';
 import Button from './Button.vue';
 import Icon from './Icon.vue';
 import ButtonGroup from './ButtonGroup.vue';
@@ -13,3 +15,70 @@ new Vue({
     isLoading: true,
   },
 });
+
+// unit test
+const expect = chai.expect;
+chai.use(spies);
+{
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting',
+    },
+  });
+  vm.$mount();
+  let useElement = vm.$el.querySelector('use');
+  const href = useElement.getAttribute('xlink:href');
+  expect(href).to.equal('#i-setting');
+  vm.$el.remove();
+  vm.$destroy();
+}
+{
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting',
+      loading: true,
+    },
+  });
+  vm.$mount();
+  let useElement = vm.$el.querySelector('use');
+  const href = useElement.getAttribute('xlink:href');
+  expect(href).to.equal('#i-loading');
+  vm.$el.remove();
+  vm.$destroy();
+}
+
+{
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting',
+    },
+  });
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  vm.$mount(div);
+  let svg = vm.$el.querySelector('svg');
+  const { order } = window.getComputedStyle(svg);
+  expect(order).to.equal('1');
+  vm.$el.remove();
+  vm.$destroy();
+}
+
+{
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting',
+    },
+  });
+  vm.$mount();
+  const spy = chai.spy(() => {});
+  vm.$on('click', spy);
+  let button = vm.$el;
+  button.click();
+  expect(spy).to.have.been.called();
+  vm.$el.remove();
+  vm.$destroy();
+}
