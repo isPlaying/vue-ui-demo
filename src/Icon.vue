4d9a12225cee4d1f19a8ad77c086549e968421fd
isPlaying <369370@gmail.com>
2020-09-25 13:38:19 +0800

    feat(*):  新增Icon组件

---
+++ src/Icon.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg class="v-icon">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+<script>
+export default {
+  props: ["name"],
+};
+</script>
+<style lang="less">
+.v-icon {
+  width: 1em;
+  height: 1em;
+}
+</style>
--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 import Button from './Button.vue';
-
+import Icon from './Icon.vue';
 Vue.component('v-button', Button);
+Vue.component('v-icon', Icon);
 
 new Vue({
-    el: '#app',
-    data: {
-        message: 'hello world',
-    },
+  el: '#app',
+  data: {
+    message: 'hello world',
+  },
 });
