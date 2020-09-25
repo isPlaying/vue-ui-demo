7bb3ed29973b045591c5ac68a11fe949a1c6d7fe
isPlaying <369370@gmail.com>
2020-09-25 15:40:27 +0800

    feat(*):  添加v-button-group组件, 并且添加了子元素的验证

---
+++ src/ButtonGroup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="v-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    for (let child of this.$el.children) {
+      let name = child.nodeName.toLowerCase();
+      if (name !== "button") {
+        console.warn(
+          `v-button-group的子元素应该全是button，但是现在包含了${name}`
+        );
+      }
+    }
+  },
+};
+</script>
+
+<style lang="less">
+.v-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+  & > .v-button {
+    border-radius: 0;
+    margin-left: -1px;
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    &:hover {
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
+</style>
--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import Vue from 'vue';
 import Button from './Button.vue';
 import Icon from './Icon.vue';
+import ButtonGroup from './ButtonGroup.vue';
 Vue.component('v-button', Button);
 Vue.component('v-icon', Icon);
+Vue.component('v-button-group', ButtonGroup);
 
 new Vue({
   el: '#app',
   data: {
     message: 'hello world',
-    isLoading: false,
+    isLoading: true,
   },
 });
